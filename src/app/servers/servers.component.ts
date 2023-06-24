f7bbdb8bbf66ad1e4754b29d9eaf73131447b522
Zachary Delcoco <zdelcoco@gmail.com>
2023-06-24 15:45:16 -0500

    added server(s) component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created.";
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server created! Name is " + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
